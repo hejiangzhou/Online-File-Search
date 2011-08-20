05896b7de680720be1ebf2d9b9eadf0103c0359d
hjz <hejiangzhou@gmail.com>
2011-08-20 15:22:47 +0800

    Fix the bug which will cause the program crash when a file in database no longer exists in filesystem.

--- cgi/findfilesconf.py
+++ cgi/findfilesconf.py
@@ -1,6 +1,6 @@
 LIMIT=200
-BASE_DIR='/home/hjz'
-BASE_URL='/smallwhite'
+BASE_DIR='/home/share'
+BASE_URL='/files'
 TIME_STYLE='%m/%d/%Y'
 DB_FILE='/var/lib/mlocate/mlocate.db'
 DEDICATED_DB_FILE=False
